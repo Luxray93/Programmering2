9b01f3ec26165e0789214b9065a0df8e2e9fa851
Luxray93 <john117654@gmail.com>
2019-11-08 13:51:35 +0100

    gssgd

---
+++ src/abstractclasses/Volvo.java
@@ -0,0 +1,22 @@
+package abstractclasses;
+
+public class Volvo extends Cars{
+
+	public Volvo() {
+	}
+		@Override
+		void speed() {
+			System.out.println("250 km/h");
+		}
+		@Override
+		void acceleration() {
+			System.out.println("0-100 7 sec");
+		}
+		@Override
+		void mileage() {
+			System.out.println("2500 mil");
+		}
+		
+}
+	
+	
